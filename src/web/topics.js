26a2aa53f7c5ac4f84991c03e86a774a67eae490
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-04-22 13:38:45 -0500

    Add a list of topic categorities from de open directory DMOZ

---
+++ src/web/topics.js
@@ -0,0 +1,27 @@
+/**
+ * List of topics for the search engine
+ * This file contains the list of topics that are used in the search form of the web crawler.
+ * The topics are used to build the URL of the search engine.
+ * The topics was obtained from the Open Directory Project (ODP) at http://dmoz.org.
+ * 
+ * @type {string[]}
+ * @see {@link https://en.wikipedia.org/wiki/Open_Directory_Project|Open Directory Project}
+ */
+export const topics = [
+  "art",
+  "business",
+  "computers",
+  "games",
+  "health",
+  "home",
+  "news",
+  "recreation",
+  "reference",
+  "regional",
+  "science",
+  "shopping",
+  "society",
+  "sports",
+  "kids and teens",
+  "world",
+];
