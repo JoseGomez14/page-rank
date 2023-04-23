7c5c7c2cfff19c4bce1a135022d40aedde81f1a4
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-04-22 23:36:03 -0500

    Add search and view for the results and recomendations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
+import { SearchIcon } from "@/components/iconos";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [pageRank, setPageRank] = useState({});
+  const [error, setError] = useState(null);
+  const [recomendations, setRecomendations] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+
+  useEffect(() => {
+    fetch("/api/pageRank")
+      .then((res) => res.json())
+      .then((data) => setRecomendations(data.pageRank.slice(0, 5)));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const query = e.target.query.value.trim();
+    if (query) {
+      fetch(`/api/pageRank?query=${query}`)
+        .then((res) => res.json())
+        .then((data) => setPageRank(data));
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setInputSearch(value);
+    if (!value) {
+      setPageRank({});
+    }
+  };
+
   return (
     <>
       <Head>
@@ -15,30 +46,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
         <div className={styles.center}>
           <Image
             className={styles.logo}
@@ -49,66 +56,48 @@ export default function Home() {
             priority
           />
         </div>
-
+        <form className={styles.search} onSubmit={handleSubmit}>
+          <input
+            id="query"
+            name="query"
+            value={inputSearch}
+            onChange={handleInputChange}
+            className={styles.input}
+            type="text"
+            placeholder="Search..."
+          />
+          <button className={styles.button} type="submit" title="Search">
+            <SearchIcon />
+          </button>
+        </form>
+        <br />
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          {pageRank.pageRank
+            ? pageRank.pageRank.map(({ value, title, description }, index) => (
+                <div key={index} className={styles.card}>
+                  <h2 className={inter.className}>{title}</h2>
+                  <p className={inter.className}>{description}</p>
+                  <p className={inter.className}>PR: {value.toFixed(4)}</p>
+                </div>
+              ))
+            : recomendations.length > 0 && (
+                <>
+                  <h2 className={inter.className}>Recomended Pages</h2>
+                  {recomendations.map(
+                    ({ value, title, description }, index) => (
+                      <div key={index} className={styles.card}>
+                        <h2 className={inter.className}>{title}</h2>
+                        <p className={inter.className}>{description}</p>
+                        <p className={inter.className}>
+                          PR: {value.toFixed(4)}
+                        </p>
+                      </div>
+                    )
+                  )}
+                </>
+              )}
         </div>
       </main>
     </>
-  )
+  );
 }
