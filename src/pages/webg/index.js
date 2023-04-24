d846949cdff24cdadf5e3e090f4ba704f5571d35
JoseGomez14 <jose.gomez14@udea.edu.co>
2023-04-23 22:28:31 -0500

    Add back link

--- src/pages/webg/index.js
+++ src/pages/webg/index.js
@@ -1,15 +1,20 @@
-import styles from '@/styles/Home.module.css'
-import Graph from '@/components/Graph';
-import { Inter } from 'next/font/google';
+import styles from "@/styles/Home.module.css";
+import Graph from "@/components/Graph";
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function webg() {
-  return <main className={styles.main} style={{ padding: '3rem 1rem'}}>
-    <h1 className={inter.className} style={{ marginBottom: '2rem'}}>The Web Graph</h1>
-    <Graph />
-  </main>;
+  return (
+    <main className={styles.main} style={{ padding: "3rem 1rem" }}>
+      <h1 className={inter.className} style={{ marginBottom: "2rem" }}>
+        <Link href="/">&larr; </Link>
+        The Web Graph
+      </h1>
+      <Graph />
+    </main>
+  );
 }
 
 export default webg;
-
